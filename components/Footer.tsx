71f42e3cc8a1f75707cf5f45b742c796d829380b
ardooz-lol <231712234@students.uajy.ac.id>
2025-04-21 15:15:12 +0700

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-100 mt-10 text-center p-4 text-sm text-gray-500">
+        © 2025 TolOng. All rights reserved.
+      </footer>
+    )
+  }
+  
